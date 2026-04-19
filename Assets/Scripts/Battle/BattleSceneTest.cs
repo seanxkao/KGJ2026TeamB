90124a9da659df2be336aaf92e33f285dd00f582
seankao <seankao@rayark.com>
2026-04-19 14:44:48 +0800

    Add battle result ui and back to main menu.

--- Assets/Scripts/Battle/BattleSceneTest.cs
+++ Assets/Scripts/Battle/BattleSceneTest.cs
@@ -1,58 +1,6 @@
-using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.UI;
 
+[System.Obsolete("BattleSceneTest has been replaced by BattleResultUI. Remove this component from scenes.")]
 public class BattleSceneTest : MonoBehaviour
 {
-    [SerializeField]
-    private Button _restartButton;
-    [SerializeField]
-    private BattleManager _battleManager;
-
-    private void Start()
-    {
-        if (_restartButton != null)
-        {
-            _restartButton.onClick.AddListener(_Restart);
-        }
-    }
-
-    private void OnDestroy()
-    {
-        if (_restartButton != null)
-        {
-            _restartButton.onClick.RemoveListener(_Restart);
-        }
-    }
-
-    private void _Restart()
-    {
-        RestartAsync().Forget();
-    }
-
-    private async UniTaskVoid RestartAsync()
-    {
-        if (_battleManager == null)
-        {
-            Debug.LogWarning("BattleSceneTest is missing a BattleManager reference.", this);
-            return;
-        }
-
-        if (_restartButton != null)
-        {
-            _restartButton.interactable = false;
-        }
-
-        try
-        {
-            await _battleManager.RestartAsync();
-        }
-        finally
-        {
-            if (_restartButton != null)
-            {
-                _restartButton.interactable = true;
-            }
-        }
-    }
 }
